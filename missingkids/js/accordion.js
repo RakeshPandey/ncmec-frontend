67b3cbacac4f60338ccbcd603cb75eb47ee4f8c6
Rakesh Pandey <rakpande@adobe.com>
2017-07-17 12:40:19 +0530

    Commit latest HTML

---
+++ missingkids/js/accordion.js
@@ -0,0 +1,11 @@
+/*******************************
+* ACCORDION WITH TOGGLE ICONS
+*******************************/
+	function toggleIcon(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find(".more-less")
+            .toggleClass('fa-plus-circle fa-minus-circle');
+    }
+    $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+    $('.panel-group').on('shown.bs.collapse', toggleIcon);
