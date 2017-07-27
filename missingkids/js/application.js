f32d8185705bf2a31ef7f433a46f90d6d0647e75
Hanush H Nair <hnair@adobe.com>
2017-07-27 16:22:16 +0530

    header

--- missingkids/js/application.js
+++ missingkids/js/application.js
@@ -1,11 +1,55 @@
-$(function() {
+$(function () {
     var $divs = $("#js-forms-content > div");
     $divs.first().show()
-    $("input[name$='ncmec-forms-online']").on("change", function() {
-            $divs.hide();
-            $divs.eq( $("input[name$='ncmec-forms-online']").index( this ) ).show();
+    $("input[name$='ncmec-forms-online']").on("change", function () {
+        $divs.hide();
+        $divs.eq($("input[name$='ncmec-forms-online']").index(this)).show();
     });
 
     $('.match-height').matchHeight({byRow: false})
+
+
+
+    window.onresize = function () {
+
+        
+        ///////////////////////////////////////////////////////////////////
+        /** Make desktop header navigation hoverable instead of click **/
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+            $('#navbar > ul > li').hover(
+                    function () {
+                        $(this).addClass('open')
+                    },
+                    function () {
+                        $(this).removeClass('open')
+                    }
+            );
+        }
+        /** -- END -- Make desktop header navigation hoverable instead of click **/
+        ////////////////////////////////////////////////////////////////////
+    }
+    
+    
+    
+    function onScroll() {
+        var $body = $('body');
+            if (document.body.scrollTop > 1) {
+
+                if (!$body.hasClass('scroll-started')) {
+                    $body.addClass('scroll-started');
+                }
+
+            } else {
+                if ($body.hasClass('scroll-started')) {
+                    $body.removeClass('scroll-started');
+                }
+            }
+
+
+
+        
+    }
+    $(window).scroll(onScroll);
+    onScroll();
 
 });
