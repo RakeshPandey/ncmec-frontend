cdca841b7ed8681fa1a3ced75e9bc2c2cf9e8c31
Rakesh Pandey <rakpande@adobe.com>
2017-07-31 12:18:06 +0530

    Added Jquery Validations on form Pages

--- missingkids/js/validation.js
+++ missingkids/js/validation.js
@@ -62,5 +62,109 @@ $().ready(function() {
             additional_info: "This field is required"
         }
     });
+
+    /* ===== ICAAN Form Starts Here ===== */
+
+    $("#js-icaan-form").validate({
+        rules: {
+            name: "required", 
+            firm: "required",
+            address: "required",
+            city: "required",
+            state: "required",
+            zip: "required",
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            country_of_interest: "required",
+            federal_courts: "required",
+            language: "required"
+        },
+        messages: {
+            name: "This field is required",
+            firm: "This field is required",
+            address: "This field is required",
+            city: "This field is required",
+            state: "This field is required",
+            zip: "This field is required",
+            phone: "This field is required",
+            email: "This field is required",
+            country_of_interest: "This field is required",
+            federal_courts: "This field is required",
+            language: "This field is required"
+        }
+    });
+
+    /* ===== Donor Privacy Form Starts Here ===== */
+
+    $("#js-donor-privacy").validate({
+        rules: {
+            name: "required", 
+            email: {
+                required: true,
+                email: true
+            },
+            your_request: "required"
+        },
+        messages: {
+            name: "This field is required",
+            email: "This field is required",
+            your_request: "This field is required"
+        }
+    });
+
+    /* ===== Contact Us Form Starts Here ===== */
+
+    $("#js-contact-us").validate({
+        rules: {
+            contact_name: "required", 
+            contact_email: {
+                required: true,
+                email: true
+            },
+            comments: "required"
+        },
+        messages: {
+            contact_name: "This field is required",
+            contact_email: "This field is required",
+            comments: "This field is required"
+        }
+    });
+
+    /* ===== Enews Form Starts Here ===== */
+
+    $("#js-enews").validate({
+        rules: { 
+            contact_email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            contact_email: "This field is required"
+        }
+    });
+
+    /* ===== Media Connect Form Starts Here ===== */
+
+    $("#js-media-connect").validate({
+        rules: { 
+            email: {
+                required: true,
+                email: true
+            },
+            fullname: "required",
+            city: "required",
+            state: "required"
+        },
+        messages: {
+            email: "This field is required",
+            fullname: "This field is required",
+            city: "This field is required",            
+            state: "This field is required"
+        }
+    });
     
 });
