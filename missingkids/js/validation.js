0b79ff02399629b9918ee85d2f3a63f8e9e6e5bd
Rakesh Pandey <rakpande@adobe.com>
2017-08-01 18:19:09 +0530

    Phone, zip and email validation rules

--- missingkids/js/validation.js
+++ missingkids/js/validation.js
@@ -24,8 +24,7 @@ $().ready(function() {
         },
         messages: {
             firstname: "Please enter your firstname",
-            lastname: "Please enter your lastname",
-            email: "Please enter a valid email address"
+            lastname: "Please enter your lastname"
         }
     });
 
@@ -41,7 +40,12 @@ $().ready(function() {
             city: "required",
             state: "required",
             zip: "required",
-            phone: "required",
+            phoneNumber: {
+                phoneUS: true,
+                required: true,
+                minlength : 10,
+                maxlength : 10
+            },
             email: {
                 required: true,
                 email: true
@@ -56,9 +60,6 @@ $().ready(function() {
             address: "This field is required",
             city: "This field is required",
             state: "This field is required",
-            zip: "This field is required",
-            phone: "This field is required",
-            email: "This field is required",
             additional_info: "This field is required"
         }
     });
@@ -77,7 +78,12 @@ $().ready(function() {
             city_1: "required",
             state_1: "required",
             zip_1: "required",
-            phone_1: "required",
+            phoneNumber: {
+                phoneUS: true,
+                required: true,
+                minlength : 10,
+                maxlength : 10
+            },
             email_1: {
                 required: true,
                 email: true
@@ -95,10 +101,7 @@ $().ready(function() {
             agency_dept_1: "This field is required",
             address_1: "This field is required",
             city_1: "This field is required",
-            state_1: "This field is required",
-            zip_1: "This field is required",
-            phone_1: "This field is required",
-            email_1: "This field is required",
+            state_1: "This field is required",            
             child_policy_1: "This field is required",
             jurix_population: "This field is required",
             sworn_officers: "This field is required"
@@ -115,7 +118,12 @@ $().ready(function() {
             city: "required",
             state: "required",
             zip: "required",
-            phone: "required",
+            phoneNumber: {
+                phoneUS: true,
+                required: true,
+                minlength : 10,
+                maxlength : 10
+            },
             email: {
                 required: true,
                 email: true
@@ -130,9 +138,6 @@ $().ready(function() {
             address: "This field is required",
             city: "This field is required",
             state: "This field is required",
-            zip: "This field is required",
-            phone: "This field is required",
-            email: "This field is required",
             country_of_interest: "This field is required",
             federal_courts: "This field is required",
             language: "This field is required"
@@ -152,7 +157,6 @@ $().ready(function() {
         },
         messages: {
             name: "This field is required",
-            email: "This field is required",
             your_request: "This field is required"
         }
     });
@@ -173,7 +177,6 @@ $().ready(function() {
         },
         messages: {
             contact_name: "This field is required",
-            contact_email: "This field is required",
             comments: "This field is required"
         }
     });
@@ -188,7 +191,7 @@ $().ready(function() {
             }
         },
         messages: {
-            contact_email: "This field is required"
+
         }
     });
 
@@ -205,7 +208,6 @@ $().ready(function() {
             state: "required"
         },
         messages: {
-            email: "This field is required",
             fullname: "This field is required",
             city: "This field is required",            
             state: "This field is required"
