b91d0cb589135620a8307ae295b33ab8b3625076
Hanush H Nair <hnair@adobe.com>
2017-08-01 17:20:28 +0530

    ajax driven

---
+++ missingkids/js/search-results.js
@@ -0,0 +1,56 @@
+(function ($, Handlebars) {
+
+
+
+
+    var _this, searchMod = {
+        el: {
+            searchBtn: $("#searchBtn"),
+            siteSearchResults: $("#siteSearchResults")
+        },
+        templates: {
+            searchItem: Handlebars.compile($("#search-result-item").html())
+        },
+        init: function () {
+            _this = this;
+
+            _this.initHandlebarRegisters();
+            _this.el.searchBtn.click(_this.performSearch);
+            _this.el.siteSearchResults.on('click', '.pagination a', _this.performSearch);
+        },
+        performSearch: function (e) {
+            if (e) {
+                e.preventDefault();
+            }
+            _this.el.siteSearchResults.empty().html("<p>Loading..</p>");
+            $.get('./missingkids/js/search-results.json', function (data) {
+                var html = _this.templates.searchItem(data);
+                _this.el.siteSearchResults.empty().html(html);
+            });
+        },
+
+        initHandlebarRegisters: function () {
+
+            Handlebars.registerHelper("inc", function (value, options)
+            {
+                return parseInt(value) + 1;
+            });
+
+            Handlebars.registerHelper('ifPdf', function (v1,  options) {
+                if (v1 === "pdf") {
+                    return options.fn(this);
+                }
+                return options.inverse(this);
+            });
+
+        }
+
+
+    };
+
+
+    searchMod.init();
+
+
+
+})(jQuery, Handlebars);
