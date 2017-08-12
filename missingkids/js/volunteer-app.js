90b11d899a63ae1b41e5aa69ea0506eafd7dd174
Subash Selvaraj <sselvara@adobe.com>
2017-08-13 02:35:30 +0530

    update

--- missingkids/js/volunteer-app.js
+++ missingkids/js/volunteer-app.js
@@ -1,20 +1,27 @@
 (function(){
 
-  // personal Info form validation handler
+  // form validation handler
   $('.volunteerAppAccordion form').each(function($index, $form){
 
     $($form).validate({
       submitHandler: function(form) {
-        //$(form).submit();
         var panelWrapper = $(form).parents('.panel');
         panelWrapper.addClass('has-valid-form');
         var len = panelWrapper.next('.panel').length;
         if(len){
           panelWrapper.next('.panel').find('.panel-heading a').trigger('click');
         }
+        else {
+          //============================
+          // Add your submit logic here
+          //============================
+
+        }
       }
     });
   });
+
+
   // $('.volunteerAppAccordion').on('hidden.bs.collapse', function (e) {
   //   var panelWrapper = $(e.target).parents('.panel');
   //   //panelWrapper.removeClass('has-valid-form');
