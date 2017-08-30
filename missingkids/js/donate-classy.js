c99db07c997a1f8c0c515ceb5aa17dc150e23dad
soubasu <soubasu@adobe.com>
2017-08-30 19:27:36 +0530

    Classy Donation Component

---
+++ missingkids/js/donate-classy.js
@@ -0,0 +1,14 @@
+(function(){
+    
+    $(document).ready(function(){
+        $('.donate').click(function(){
+            $('.donate.btn-primary').removeClass('btn-primary').addClass('btn-secondary');
+            $(this).addClass('btn-primary').removeClass('btn-secondary');
+            
+            var amt = $(this).text();            
+            var num = (amt == 'Others')?'':parseInt(amt.match(/\d+/),10);
+            $('#dollars').val(num);
+        });
+    });
+    
+})();
